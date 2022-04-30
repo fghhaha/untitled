08007fa374b7c41d6dae0087f65ef43c37768e97
fgh <2890858968@qq.com>
2022-04-30 14:47:20 +0800

    第二次

--- src/main/java/test/gittest.java
+++ src/main/java/test/gittest.java
@@ -5,5 +5,6 @@ import javax.sound.midi.Soundbank;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("heelo git");
+        System.out.println("hello git2");
     }
 }
